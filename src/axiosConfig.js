7ad9578b5f1be30bdbea35b3823e6f313db3ccde
Usmanwasif123 <usmanwasif8@gmail.com>
2024-02-03 20:03:03 +0500

    changes added

---
+++ src/axiosConfig.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const token = sessionStorage.getItem('token');
+
+axios.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default axios;
