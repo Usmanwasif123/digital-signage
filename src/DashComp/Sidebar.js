6ebf545c088961dfeefe4a139879a3a178fd43c9
Usmanwasif123 <usmanwasif8@gmail.com>
2024-02-07 14:01:43 +0500

    changes in the sidebar components

--- src/DashComp/Sidebar.js
+++ src/DashComp/Sidebar.js
@@ -18,14 +18,19 @@ const Sidebar = ({onShowScreens}) => {
         <li className='signout' >
         <Link to="/signin" style={{color: '#fff',  }}>Sign out</Link></li>
         <li>
-        <Link to="/profile"><img className='sidebar-content' src='https://i.ibb.co/st71q5V/Asset-6.png'/></Link></li>
+        <Link to="/content"><img className='sidebar-content' src='https://i.ibb.co/st71q5V/Asset-6.png'/></Link></li>
         <li>
-        <Link to="/profile"><img className='sidebar-video' src='https://i.ibb.co/GFkdDgP/Asset-7.png'/></Link></li>
+          <li>
+          <button className='show-screen' onClick={onShowScreens} style={{ top: '10px', marginLeft:'-810px', zIndex: '5', background: 'transparent', position: 'absolute'}}><img className='sidebar-screen'  
+      src='https://i.ibb.co/5KJ2HPJ/Asset-5.png'/></button>
+          </li>
+        <Link to="/loop"><img className='sidebar-video' src='https://i.ibb.co/GFkdDgP/Asset-7.png'/></Link></li>
         <li>
-        <Link to="/profile"><img className='sidebar-schedule' src='https://i.ibb.co/8jFLw3g/Asset-8.png'/></Link></li>
+        <Link to="/schedule"><img className='sidebar-schedule' src='https://i.ibb.co/8jFLw3g/Asset-8.png'/></Link></li>
 
         <li>
-        <Link to="/profile"><img className='sidebar-chat' src='https://i.ibb.co/MVtW40s/image.png'/></Link></li>
+        <Link to="/" ><img className='sidebar-chat' src='https://i.ibb.co/MVtW40s/image.png'/></Link></li>
+        
       </ul>
     </div>
   )
